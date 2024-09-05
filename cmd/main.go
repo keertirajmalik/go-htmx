3ab36199b432285474ad5f7901f4999470dfce27
KeertirajMalik <keertirajlaxman.malik@imtf.com>
2024-09-05 13:11:51 +0530

    fix: html template issues

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"html/template"
+	"io"
 	"log"
 	"net/http"
 	"sync"
 )
 
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func newTemplates() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
 
 type Count struct {
 	Count int
@@ -21,26 +36,26 @@ func (c *Count) Increment() {
 
 func handleGet(templates *Templates, count *Count) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		templates.Render(w, "index", count)
+		templates.Render(w, "count", count)
 	}
 }
 
 func handleCountInc(templates *Templates, count *Count) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		count.Count++
-		templates.Render(w, "count", count)
+		templates.Render(w, "count-div", count)
 	}
 }
 
 func main() {
 	const port = "8080"
 
-	templates := newTemplates()
-	count := &Count{Count: 0}
+    templates := newTemplates()
+    count := &Count{Count: 0}
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /", handleGet(templates, count))
+	mux.HandleFunc("GET /count", handleGet(templates, count))
 	mux.HandleFunc("POST /count", handleCountInc(templates, count))
 
 	server := &http.Server{
