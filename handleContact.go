047fd0c56e4d3f94168358be944b897576bdc035
KeertirajMalik <keertirajlaxman.malik@imtf.com>
2024-09-05 17:33:05 +0530

    feat:add email validation

--- handleContact.go
+++ handleContact.go
@@ -1,3 +1,93 @@
 package main
 
+import "net/http"
 
+type Contact struct {
+	Name  string
+	Email string
+}
+
+func newContact(name, email string) Contact {
+	return Contact{
+		Name:  name,
+		Email: email,
+	}
+}
+
+type Contacts = []Contact
+
+type Data struct {
+	Contacts Contacts
+}
+
+func (d *Data) hasEmail(email string) bool {
+	for _, contact := range d.Contacts {
+		if contact.Email == email {
+			return true
+		}
+	}
+	return false
+}
+
+func newData() Data {
+	return Data{
+		Contacts: []Contact{
+			newContact("John", "jd@test.com"),
+			newContact("Clara", "cd@test.com"),
+		},
+	}
+}
+
+type FormData struct {
+	Values map[string]string
+	Errors map[string]string
+}
+
+func newFormData() FormData {
+	return FormData{
+		Values: make(map[string]string),
+		Errors: make(map[string]string),
+	}
+}
+
+type Page struct {
+	Data Data
+	Form FormData
+}
+
+func newPage() Page {
+	return Page{
+		Data: newData(),
+		Form: newFormData(),
+	}
+}
+
+func handleContactGet(templates *Templates, page Page) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		templates.Render(w, "contact", page)
+	}
+}
+
+func handleContactCreate(templates *Templates, page Page) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name := r.FormValue("name")
+		email := r.FormValue("email")
+
+		if page.Data.hasEmail(email) {
+			formData := newFormData()
+			formData.Values["name"] = name
+			formData.Values["email"] = email
+			formData.Errors["email"] = "Email already exists"
+
+			w.WriteHeader(http.StatusUnprocessableEntity)
+			templates.Render(w, "contact-form", formData)
+			return
+		}
+
+		contact := newContact(name, email)
+		page.Data.Contacts = append(page.Data.Contacts, contact)
+
+		templates.Render(w, "contact-form", newFormData())
+		templates.Render(w, "oob-contact-div", contact)
+	}
+}
--- main.go
+++ main.go
@@ -5,52 +5,21 @@ import (
 	"net/http"
 )
 
-type Contact struct {
-	Name  string
-	Email string
-}
-
-func newContact(name, email string) Contact {
-	return Contact{
-		Name:  name,
-		Email: email,
-	}
-}
-
-type Contacts = []Contact
-
-type Data struct {
-	Contacts Contacts
-}
-
-func newData() Data {
-	return Data{
-		Contacts: []Contact{
-			newContact("John", "jd@test.com"),
-			newContact("Clara", "cd@test.com"),
-		},
-	}
-}
-
-func handleContactGet(templates *Templates, data Data) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		templates.Render(w, "contact", data)
-	}
-}
-
 func main() {
 	const port = "8080"
 
 	templates := newTemplates()
 	count := &Count{Count: 0}
-    data := newData()
+
+    page := newPage()
 
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /count", handleCountGet(templates, count))
 	mux.HandleFunc("POST /count", handleCountInc(templates, count))
 
-	mux.HandleFunc("GET /contact", handleContactGet(templates, data))
+	mux.HandleFunc("GET /contact", handleContactGet(templates, page))
+	mux.HandleFunc("POST /contact", handleContactCreate(templates, page))
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
